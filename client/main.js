eb1a7a96577f948389109a83876ce6ee8c674df2
gungui98 <gunguimooc@gmail.com>
2018-03-05 18:59:25 +0700

    switch to react
    switch to react

--- client/main.js
+++ client/main.js
@@ -1,8 +1,22 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import todosList from '../imports/components/todosList/todosList';
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
 
-angular.module('simple-todos', [
-    angularMeteor,
-    todosList.name
-]);
+import App from '../imports/ui/App.js';
+
+Meteor.startup(() => {
+    render(<App />, document.getElementById('render-target'));
+});
+
+Template.body.onRendered(function() {
+    let settings = 'pjs-settings.json';
+    this.autorun(() => {
+        if (particlesJS) {
+            console.log(`loading particles.js config from "${settings}"...`)
+            /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+            particlesJS.load('particles-js', settings, function () {
+                console.log('callback - particles.js config loaded');
+            });
+        }
+    });
+});
