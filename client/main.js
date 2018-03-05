2823e22ff3165f232ab9bd2d58b3454e2de4e55c
gungui98 <gunguimooc@gmail.com>
2018-03-05 19:45:54 +0700

    finalize demo
    finalize demo

--- client/main.js
+++ client/main.js
@@ -2,21 +2,9 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-    render(<App />, document.getElementById('render-target'));
-});
-
-Template.body.onRendered(function() {
-    let settings = 'pjs-settings.json';
-    this.autorun(() => {
-        if (particlesJS) {
-            console.log(`loading particles.js config from "${settings}"...`)
-            /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-            particlesJS.load('particles-js', settings, function () {
-                console.log('callback - particles.js config loaded');
-            });
-        }
-    });
+  render(<App />, document.getElementById('render-target'));
 });
