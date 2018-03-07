42e119c304d8c6f0afd08b8904fdb1cf3f1b3664
gungui98 <gunguimooc@gmail.com>
2018-03-07 08:52:58 +0700

    nothing

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,103 +1,45 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
+import Particles from 'react-particles-js';
 
-import { Tasks } from '../api/tasks.js';
+export default class App extends Component{
 
-import Task from './Task.js';
-import AccountsUIWrapper from './AccountsUIWrapper.js';
-
-// App component - represents the whole app
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hideCompleted: false,
-    };
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
-    Meteor.call('tasks.insert', text);
-
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-  }
-
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: !this.state.hideCompleted,
-    });
-  }
-
-  renderTasks() {
-    let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
+    constructor() {
+        super();
     }
-    return filteredTasks.map((task) => {
-      const currentUserId = this.props.currentUser && this.props.currentUser._id;
-      const showPrivateButton = task.owner === currentUserId;
-
-      return (
-        <Task
-          key={task._id}
-          task={task}
-          showPrivateButton={showPrivateButton}
-        />
-      );
-    });
-  }
-
-  render() {
+    render() {
+    const btext ={
+        color: '#f9f3f4',
+        position: 'absolute',
+        textAlign: 'center',
+        top: '40%',
+        width: '100%',
+        lineHeight: '0.4em',
+    }
+    const particle={
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#0F1331',
+    }
+    const btn = {
+        color: '#fff',
+        textDecoration: 'none',
+        border: '#ccc 1px solid',
+        padding: '10px 15px',
+        borderRadius: '8px',
+        lineHeight: '4em',
+    }
     return (
-      <div className="container">
-        <header>
-          <h1>Todo List ({this.props.incompleteCount})</h1>
-
-          <label className="hide-completed">
-            <input
-              type="checkbox"
-              readOnly
-              checked={this.state.hideCompleted}
-              onClick={this.toggleHideCompleted.bind(this)}
-            />
-            Hide Completed Tasks
-          </label>
-
-          <AccountsUIWrapper />
-
-          { this.props.currentUser ?
-            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-              <input
-                type="text"
-                ref="textInput"
-                placeholder="Type to add new tasks"
-              />
-            </form> : ''
-          }
-        </header>
-
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
+        <div >
+            <Particles style ={particle} />
+            <div style={btext}>
+                <h1>Duolingo clone</h1>
+                <p>Website is under construction</p>
+                <a href="https://github.com/gungui98/Duolingo_clone" style={btn} >Get Started</a>
+            </div>
+        </div>
     );
-  }
+    }
+
 }
-
-export default withTracker(() => {
-  Meteor.subscribe('tasks');
-
-  return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
-    currentUser: Meteor.user(),
-  };
-})(App);
